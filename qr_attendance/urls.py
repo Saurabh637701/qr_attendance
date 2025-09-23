a7987e075adba35af045d46336d8c1506a2da012
Saurabh-Saini <sainisaurabh2124@gmail.com>
2025-09-23 09:12:26 +0530

    Update urls.py

--- qr_attendance/urls.py
+++ qr_attendance/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("qr/", include("qr_app.urls")),
     path('login/', include("login.urls")),# ✅ qr_app से connect
-    path("attendance/", include("qr_app.urls"))
+    
     path('', lambda request: redirect('user_login')),
 ]
+
 
 
 
