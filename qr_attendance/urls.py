bec5f98cb499b6bb31d148f0832c2c779bf742c2
Saurabh-Saini <sainisaurabh2124@gmail.com>
2025-09-23 08:22:24 +0530

    Update urls.py

--- qr_attendance/urls.py
+++ qr_attendance/urls.py
@@ -4,9 +4,10 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("qr_app/", include("qr_app.urls")),
+    path("", include("qr_app.urls")),
     path('login/', include("login.urls")),# ✅ qr_app से connect
     path('', lambda request: redirect('user_login')),
 ]
+
 
 
